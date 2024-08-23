b41894ae657e2b682f9abc210ac3d451b4b37862
christopher-gustavson <85360986+christopher-gustavson@users.noreply.github.com>
2024-08-23 11:50:57 -0700

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,16 +1,34 @@
-const optimizelyClient = window.optimizelySdk.createInstance({
-    sdkKey: 'JnAuUBx7dZUdibLcw9kPm'
+import { createInstance, OptimizelyDecideOption } from '@optimizely/optimizely-sdk';
+
+// set global default decide options when initializing the client
+const optimizelyClient = createInstance({
+    sdkKey: 'JnAuUBx7dZUdibLcw9kPm',
+    defaultDecideOptions: [OptimizelyDecideOption.DISABLE_DECISION_EVENT],
 });
 
-optimizelyClient.onReady().then(() => {
+if (optimizely) {
+    optimizely.onReady().then(({ success, reason }) => {
+        if (!success) {
+            throw new Error(reason);
+        }
 
-    // generate a random user ID
-    let userId = 'abc123';
-    // add user attribute
-    // create a user context
-    let user = optimizelyClient.createUserContext(userId);
+        const user = optimizely.createUserContext('abc123');
+        if (!user) {
+            throw new Error('failed to create user context');
+        }
 
-    const decisionResults = user.decideAll([OptimizelyDecideOption.DISABLE_DECISION_EVENT]);
-    console.log('decisionResults', decisionResults);
+        // set additional options in a decide call
+        const decisionResults = user.decideAll(
+            [
+                OptimizelyDecideOption.ENABLED_FLAGS_ONLY,
+                OptimizelyDecideOption.IGNORE_USER_PROFILE_SERVICE,
+            ]
+        );
 
-});
+        console.log('decisionResults', decisionResults);
+    }).catch((err) => {
+        // handle error
+    });
+} else {
+    // handle instantiation error
+}
