9962a3fe20d5315ef6cff3462ca5af713d8aae80
christopher-gustavson <85360986+christopher-gustavson@users.noreply.github.com>
2024-08-23 12:00:30 -0700

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,18 +1,16 @@
-import { createInstance, OptimizelyDecideOption } from '@optimizely/optimizely-sdk';
-
 // set global default decide options when initializing the client
-const optimizelyClient = createInstance({
+const optimizelyClient = window.optimizelySdk.createInstance({
     sdkKey: 'JnAuUBx7dZUdibLcw9kPm',
     defaultDecideOptions: [OptimizelyDecideOption.DISABLE_DECISION_EVENT],
 });
 
-if (optimizely) {
-    optimizely.onReady().then(({ success, reason }) => {
+if (optimizelyClient) {
+    optimizelyClient.onReady().then(({ success, reason }) => {
         if (!success) {
             throw new Error(reason);
         }
 
-        const user = optimizely.createUserContext('abc123');
+        const user = optimizelyClient.createUserContext('abc123');
         if (!user) {
             throw new Error('failed to create user context');
         }
