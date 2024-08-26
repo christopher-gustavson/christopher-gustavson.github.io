498ca576343a0f40512721ea732eda83d6ac1bc0
christopher-gustavson <85360986+christopher-gustavson@users.noreply.github.com>
2024-08-26 11:51:30 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,47 +1,55 @@
+// set global default decide options when initializing the client
 const optimizelyClient = window.optimizelySdk.createInstance({
-    sdkKey: 'JnAuUBx7dZUdibLcw9kPm'
+    sdkKey: 'JnAuUBx7dZUdibLcw9kPm',
+    defaultDecideOptions: [window.optimizelySdk.OptimizelyDecideOption.DISABLE_DECISION_EVENT],
 });
 
-optimizelyClient.onReady().then(() => {
+if (optimizelyClient) {
+    optimizelyClient.onReady().then(({ success, reason }) => {
+        if (!success) {
+            throw new Error(reason);
+        }
+        let userId = 'abc123';
+        // add user attribute
+        let attributes;
+        // if (sessionStorage.attributes) {
+        //     attributes = JSON.parse(sessionStorage.attributes);
+        // }
+        //if (!attributes.length) {
+        attributes = { seen_test: true };
+        localStorage.setItem('attributes', JSON.stringify(attributes));
+        //}
+        // create a user context
+        let user = optimizelyClient.createUserContext(userId, attributes);
 
-    // generate a random user ID
-    let userId = 'abc123';
-    // add user attribute
-    let attributes;
-    // if (sessionStorage.attributes) {
-    //     attributes = JSON.parse(sessionStorage.attributes);
-    // }
-    //if (!attributes.length) {
-    attributes = { seen_test: true };
-    localStorage.setItem('attributes', JSON.stringify(attributes));
-    //}
-    // create a user context
-    let user = optimizelyClient.createUserContext(userId, attributes);
+        // add your feature's experiment rule key
+        let decision = user.decide('banner_test');
+        let featureEnabled = decision.enabled;
+        let variableString = decision.variables['message'];
+        let variationKey = decision.variationKey;
+        // did decision fail with a critical error?
+        if (variationKey === null) {
+            console.log(' decision error: ', decision['reasons']);
+        }
+        // flip a coin to determine if a visitor will track an event
+        let trackConversion = false;
 
-    // add your feature's experiment rule key
-    let decision = user.decide('banner_test');
-    let featureEnabled = decision.enabled;
-    let variableString = decision.variables['message'];
-    let variationKey = decision.variationKey;
-    // did decision fail with a critical error?
-    if (variationKey === null) {
-        console.log(' decision error: ', decision['reasons']);
-    }
-    // flip a coin to determine if a visitor will track an event
-    let trackConversion = false;
-
-    // log whether visitor got the feature, and which variation and feature variable they got
-    if (featureEnabled) {
-        var banner = '<div class="top-banner" style="background-color: blue;color: #fff; padding: 3px; font-weight: bold; text-align: center; font-size: 14px; height: 30px;">' + variableString + '</div>'
-        document.querySelector('#myNavbar').insertAdjacentHTML('afterbegin', banner);
-        console.log(`\nFeature test activated. User ${user.getUserId()} saw flag variation: ${variationKey} and got the message: ${variableString}`);
-        // fire a conversion event, depending on the prior coin flip
-        if (trackConversion) {
-            user.trackEvent('user_converted');
-            console.log(`\nUser ${user.getUserId()} converted.`)
+        // log whether visitor got the feature, and which variation and feature variable they got
+        if (featureEnabled) {
+            var banner = '<div class="top-banner" style="background-color: blue;color: #fff; padding: 3px; font-weight: bold; text-align: center; font-size: 14px; height: 30px;">' + variableString + '</div>'
+            document.querySelector('#myNavbar').insertAdjacentHTML('afterbegin', banner);
+            console.log(`\nFeature test activated. User ${user.getUserId()} saw flag variation: ${variationKey} and got the message: ${variableString}`);
+            // fire a conversion event, depending on the prior coin flip
+            if (trackConversion) {
+                user.trackEvent('user_converted');
+                console.log(`\nUser ${user.getUserId()} converted.`)
+            };
+        } else {
+            console.log(`\nUser ${user.getUserId()} failed traffic allocation.`)
         };
-    } else {
-        console.log(`\nUser ${user.getUserId()} failed traffic allocation.`)
-    };
-
-});
+    }).catch((err) => {
+        // handle error
+    });
+} else {
+    // handle instantiation error
+}
